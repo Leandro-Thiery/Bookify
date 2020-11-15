7029fd8509fb903fdbf3b2700ebaeacdaa123b05
Pieman47 <leandrothiery1@gmail.com>
2020-11-16 02:26:19 +0700

    First Page

--- app/src/main/java/com/example/bookify/MainActivity.java
+++ app/src/main/java/com/example/bookify/MainActivity.java
@@ -2,10 +2,12 @@ package com.example.bookify;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
         //TODO: Help later
     }
 
+
     public void onSignupBtnClick (View view){
         EditText edtTxtName = findViewById(R.id.edtTxtName);
         EditText edtTxtPass = findViewById(R.id.edtTxtPass);
@@ -27,8 +30,11 @@ public class MainActivity extends AppCompatActivity {
         TextView txtName = findViewById(R.id.splashtxtName);
         TextView txtPass = findViewById(R.id.splashTxtPass);
         TextView txtEmail = findViewById(R.id.splashTxtEmail);
+        Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show();
         txtName.setText("Name = " + edtTxtName.getText().toString());
         txtPass.setText("Password = " + edtTxtPass.getText().toString());
         txtEmail.setText("Email = " + edtTxtEmail.getText().toString());
+        Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+        startActivity(intent);
     }
 }
