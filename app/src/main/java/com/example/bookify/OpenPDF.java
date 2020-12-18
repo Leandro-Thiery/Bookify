c4dee6c2e1423753f0472c0c88fcd872747ab7ec
Pieman47 <leandrothiery1@gmail.com>
2020-12-18 22:46:32 +0700

    Prevent Screenshot (Normally)

--- app/src/main/java/com/example/bookify/OpenPDF.java
+++ app/src/main/java/com/example/bookify/OpenPDF.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.webkit.WebView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
@@ -38,6 +39,7 @@ public class OpenPDF extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_p_d_f_view);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE);
         Intent intent = getIntent();
         book = (Book) intent.getSerializableExtra("Bookpdf");
         Toast.makeText(this, book.getPdf_url(), Toast.LENGTH_SHORT).show();
