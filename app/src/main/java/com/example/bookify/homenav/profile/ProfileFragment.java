e7276be5d3fee269fada73fbd1c8ddafc81e3cd6
Pieman47 <leandrothiery1@gmail.com>
2020-12-21 14:28:19 +0700

    Remove Profile View Model

--- app/src/main/java/com/example/bookify/homenav/profile/ProfileFragment.java
+++ app/src/main/java/com/example/bookify/homenav/profile/ProfileFragment.java
@@ -42,21 +42,11 @@ public class ProfileFragment extends Fragment implements View.OnClickListener {
     private String userID;
     private DatabaseReference reference;
     private Toolbar toolbar;
-
-    private ProfileViewModel profileViewModel;
+    
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        profileViewModel =
-                ViewModelProviders.of(this).get(ProfileViewModel.class);
         View root = inflater.inflate(R.layout.fragment_profile, container, false);
-        final TextView textView = root.findViewById(R.id.text_profile);
-        profileViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
 
         logout = root.findViewById(R.id.buttonLogout);
         logout.setOnClickListener(this);
